cff362f265908703b4e8fc195280cdcd64d38117
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-06-25 18:16:21 -0300

    feat: implementando rota de listagem de produtos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,5 +4,6 @@ const productsController = require('../controllers/ProductsController')
 const productsRoutes = new Router()
 
 productsRoutes.post("/", productsController.create)
+productsRoutes.get("/", productsController.getAll)
 
 module.exports = productsRoutes
