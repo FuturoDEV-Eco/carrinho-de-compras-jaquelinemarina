02d59961703d85cf6b0a415767b03979da845cab
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-06-27 13:54:59 -0300

    feat: implementando rotas crud para clientes

--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -4,5 +4,8 @@ const clientsController = require('../controllers/ClientsController')
 const clientsRoutes = new Router()
 
 clientsRoutes.post("/", clientsController.create)
+clientsRoutes.get("/", clientsController.getAll)
+clientsRoutes.delete("/:id", clientsController.delete)
+clientsRoutes.put("/:id", clientsController.update)
 
 module.exports = clientsRoutes
