be7c70d9e87632610a3a13628a55c8b0eca64684
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-06-29 15:14:19 -0300

    refactor: implementando 'database' nas rotas de clientes e produtos

--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,11 +1,11 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const clientsController = require('../controllers/ClientsController')
 
 const clientsRoutes = new Router()
 
-clientsRoutes.post("/", clientsController.create)
-clientsRoutes.get("/", clientsController.getAll)
-clientsRoutes.delete("/:id", clientsController.delete)
-clientsRoutes.put("/:id", clientsController.update)
+clientsRoutes.post("/", clientsController.create.bind(clientsController))
+clientsRoutes.get("/", clientsController.getAll.bind(clientsController))
+clientsRoutes.delete("/:id", clientsController.delete.bind(clientsController))
+clientsRoutes.put("/:id", clientsController.update.bind(clientsController))
 
 module.exports = clientsRoutes
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,10 +3,10 @@ const productsController = require('../controllers/ProductsController')
 
 const productsRoutes = new Router()
 
-productsRoutes.post("/", productsController.create)
-productsRoutes.get("/", productsController.getAll)
-productsRoutes.get("/:id", productsController.getProductDetails)
-productsRoutes.delete("/:id", productsController.delete)
-productsRoutes.put("/:id", productsController.update)
+productsRoutes.post("/", productsController.create.bind(productsController))
+productsRoutes.get("/", productsController.getAll.bind(productsController))
+productsRoutes.get("/:id", productsController.getProductDetails.bind(productsController))
+productsRoutes.delete("/:id", productsController.delete.bind(productsController))
+productsRoutes.put("/:id", productsController.update.bind(productsController))
 
 module.exports = productsRoutes
