d028a517c4932440c9c9d7bd4d598fcdf401abd7
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-06-29 16:57:01 -0300

    feat: implementando rotas para listar e deletar pedidos

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -4,5 +4,7 @@ const ordersController = require('../controllers/OrdersController')
 const ordersRoutes = new Router()
 
 ordersRoutes.post("/", ordersController.create.bind(ordersController))
+ordersRoutes.get("/", ordersController.getAll.bind(ordersController))
+ordersRoutes.delete("/:id", ordersController.delete.bind(ordersController))
 
 module.exports = ordersRoutes
